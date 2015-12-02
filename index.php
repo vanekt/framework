be24513178eaf573c414aa4cbf39aaa2a82c2f5f
Ivan Tkachenko <ivan.tkachenko@starcode.ru>
2015-12-02 11:03:46 +0300

    Composer autoload, HttpFoundation

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/autoload.php';
+ 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+ 
+$request = Request::createFromGlobals();
+ 
+$input = $request->get('name', 'World');
+ 
+$response = new Response(sprintf('Hi %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+ 
+$response->send();
