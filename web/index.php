7d2716508b24db3529f3f3ed5a5b8ba6ad74b49c
Ivan Tkachenko <vanekt@yandex.ru>
2015-12-16 22:54:52 +0300

    controllers: begin

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-//ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
  
@@ -8,20 +8,30 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
-$request = Request::createFromGlobals();
+function render_template(Request $request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    require sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
 
+    return new Response(ob_get_clean());
+}
+
+$request = Request::createFromGlobals();
 $routes = require_once __DIR__ . '/../src/routes.php';
 
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$urlGenerator = new Routing\Generator\UrlGenerator($routes, $context);
 
 try {
-    ob_start();
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    require sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
-    $response = new Response(ob_get_clean());
+    $request->attributes->add(array_merge(
+        $matcher->match($request->getPathInfo()),
+        array(
+            'urlGenerator' => new Routing\Generator\UrlGenerator($routes, $context),
+        )
+    ));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
