660752df5db00273d7018684d756c4136887d6ac
Ivan Tkachenko <vanekt@yandex.ru>
2015-12-17 00:03:29 +0300

    ControllerResolver

--- src/routes.php
+++ src/routes.php
@@ -2,10 +2,15 @@
 
 use Symfony\Component\Routing;
 
-$routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('hello/{name}', array(
-    'name' => 'World',
-    '_controller' => function ($request) {
+class HelloController
+{
+    public function indexAction(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        return render_template($request);
+    }
+
+    public function byeAction(\Symfony\Component\HttpFoundation\Request $request)
+    {
         // $foo will be available in the template
         $request->attributes->set('foo', 'bar');
 
@@ -15,8 +20,26 @@ $routes->add('hello', new Routing\Route('hello/{name}', array(
         $response->headers->set('Content-Type', 'text/plain');
 
         return $response;
-    },
+    }
+
+    public function fooAction()
+    {
+        return new \Symfony\Component\HttpFoundation\Response('foo');
+    }
+}
+
+$routes = new Routing\RouteCollection();
+
+$routes->add('hello', new Routing\Route('hello/{name}', array(
+    '_controller' => 'HelloController::indexAction',
 )));
-$routes->add('bye', new Routing\Route('bye'));
+
+$routes->add('bye', new Routing\Route('bye', array(
+    '_controller' => 'HelloController::byeAction',
+)));
+
+$routes->add('foo', new Routing\Route('foo', array(
+    '_controller' => 'HelloController::fooAction',
+)));
 
 return $routes;
--- web/index.php
+++ web/index.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
 
 function render_template(Request $request)
 {
@@ -23,6 +24,7 @@ $routes = require_once __DIR__ . '/../src/routes.php';
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$resolver = new HttpKernel\Controller\ControllerResolver();
 
 try {
     $request->attributes->add(array_merge(
@@ -31,7 +33,11 @@ try {
             'urlGenerator' => new Routing\Generator\UrlGenerator($routes, $context),
         )
     ));
-    $response = call_user_func($request->attributes->get('_controller'), $request);
+
+    $controller = $resolver->getController($request);
+    $arguments = $resolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
