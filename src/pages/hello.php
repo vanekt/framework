f8afc21318a921c7eb8a8c72734a75e04bf0096a
Ivan Tkachenko <vanekt@yandex.ru>
2015-12-16 22:22:19 +0300

    use Symfony\Component\Routing

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,1 +1,14 @@
+<?php
+
+use Symfony\Component\Routing;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+$linkForVasya = $urlGenerator->generate(
+    'hello',
+    array('name' => 'Vasya'),
+    UrlGeneratorInterface::ABSOLUTE_URL
+);
+
+?>
 Hi <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
+<p><a href="<?= $linkForVasya  ?>">Vasya's link</a></p>
--- web/index.php
+++ web/index.php
@@ -1,27 +1,31 @@
 <?php
 
+//ini_set('display_errors', 1);
+
 require_once __DIR__ . '/../vendor/autoload.php';
  
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
- 
+use Symfony\Component\Routing;
+
 $request = Request::createFromGlobals();
-$response = new Response();
 
-$map = array(
-    '/hello' => 'hello',
-);
+$routes = include __DIR__ . '/../src/routes.php';
 
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$urlGenerator = new Routing\Generator\UrlGenerator($routes, $context);
+
+try {
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $map[$path]);
-    $response->setContent(ob_get_clean());
-} else {
-    $response
-        ->setStatusCode(404)
-        ->setContent('Not Found');
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+    $response = new Response(ob_get_clean());
+} catch (Routing\Exception\ResourceNotFoundException $e) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $e) {
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();
