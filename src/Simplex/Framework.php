7e15e48ba91b8ecd7bd3f2457940bcaf44b22ab8
Ivan Tkachenko <vanekt@yandex.ru>
2015-12-19 15:00:40 +0300

    tests

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -3,8 +3,8 @@
 namespace Simplex;
 
 use Symfony\Component\Routing;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,7 +13,7 @@ class Framework
     protected $matcher;
     protected $resolver;
 
-    public function __construct(UrlMatcher $matcher, ControllerResolver $resolver)
+    public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $resolver)
     {
         $this->matcher = $matcher;
         $this->resolver = $resolver;
@@ -30,7 +30,7 @@ class Framework
             return call_user_func_array($controller, $arguments);
         } catch (Routing\Exception\ResourceNotFoundException $e) {
             return new Response('Not Found', 404);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             return new Response('An error occurred', 500);
         }
     }
